c3154f335d19b6c11ef7f704de7b4ff8e7ae6dd2
IamYeong <wjdrhkddud2@gmail.com>
2021-05-07 18:36:14 +0900

    Splash Activity edit

--- app/src/main/java/com/iamyeong/myfriendsplace/SplashActivity.java
+++ app/src/main/java/com/iamyeong/myfriendsplace/SplashActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import com.kakao.sdk.user.model.User;
 
@@ -11,14 +13,16 @@ import java.util.ArrayList;
 
 public class SplashActivity extends AppCompatActivity implements OnGetUserNameListener, OnGetPostsListener {
 
-    FirestoreManager firestoreManager;
-    UserManager userManager;
+    private FirestoreManager firestoreManager;
+    private UserManager userManager;
     private int loadCount = 0;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        handler = new Handler();
 
         firestoreManager = FirestoreManager.getInstance(SplashActivity.this);
         userManager = UserManager.getInstance();
@@ -26,22 +30,56 @@ public class SplashActivity extends AppCompatActivity implements OnGetUserNameLi
         userManager.loadUserName(SplashActivity.this);
 
 
-        while (loadCount == 2) {
-            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
-        }
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                handler = new Handler(getMainLooper());
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        while (loadCount != 2) {
+                            System.out.println(loadCount);
+                        }
+
+                        /*
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                         */
+
+                    }
+                });
+
+            }
+        });
+
+
+        thread.start();
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
 
     }
 
     @Override
     public void onGetUserName() {
         loadCount++;
+        System.out.println("loadCount : " + loadCount);
+
     }
 
     @Override
     public void onGetPosts(ArrayList<Post> postList) {
         loadCount++;
+        System.out.println("loadCount : " + loadCount);
     }
 
 
