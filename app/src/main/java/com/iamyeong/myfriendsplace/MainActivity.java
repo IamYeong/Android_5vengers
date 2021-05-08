3c6a1de810cc7dbb5915f4a212fc29176dc48491
IamYeong <wjdrhkddud2@gmail.com>
2021-05-08 18:27:43 +0900

    Firebase Crashlytics test complete

--- app/src/main/java/com/iamyeong/myfriendsplace/MainActivity.java
+++ app/src/main/java/com/iamyeong/myfriendsplace/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
